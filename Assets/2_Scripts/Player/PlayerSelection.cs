72618ebde3d1e564b59a1079ccb91a8617d87beb
juno11234 <cozam2@naver.com>
2025-09-25 18:54:08 +0900

    플레이어 셀렉션 개선
    링큐 중복검사와 해쉬셋 자료구조로 변경

--- Assets/2_Scripts/Player/PlayerSelection.cs
+++ Assets/2_Scripts/Player/PlayerSelection.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections.Generic;
+using System.Linq; // LINQ를 사용하여 리스트에서 null을 쉽게 제거하기 위해 추가
 using UnityEngine;
 
 public class PlayerSelection : MonoBehaviour
 {
-    private List<PlayerUnit> _selectedPlayers = new List<PlayerUnit>();
+    // List 대신 HashSet을 사용하여 중복을 방지하고 추가/제거/검색 성능을 향상
+    private HashSet<PlayerUnit> _selectedPlayers = new HashSet<PlayerUnit>();
     private Dictionary<int, List<PlayerUnit>> _unitDesignations = new Dictionary<int, List<PlayerUnit>>();
 
     [Header("Move Settings")]
@@ -12,12 +15,14 @@ public class PlayerSelection : MonoBehaviour
     // 유닛 선택
     public void SelectUnit(PlayerUnit playerUnit, bool isShiftPressed)
     {
-        if (!isShiftPressed)
-            DeselectAllUnit();
-
-        if (!_selectedPlayers.Contains(playerUnit))
+        if (isShiftPressed == false)
         {
-            _selectedPlayers.Add(playerUnit);
+            DeselectAllUnit();
+        }
+
+        // HashSet의 Add 메서드는 중복된 항목이 없을 때만 true를 반환
+        if (_selectedPlayers.Add(playerUnit))
+        {
             playerUnit.Selected();
         }
     }
@@ -25,8 +30,12 @@ public class PlayerSelection : MonoBehaviour
     public void DeselectAllUnit()
     {
         foreach (var player in _selectedPlayers)
-            player.deSelected();
-
+        {
+            if (player != null) // 유닛이 파괴되었을 경우를 대비
+            {
+                player.deSelected();
+            }
+        }
         _selectedPlayers.Clear();
     }
 
@@ -34,51 +43,67 @@ public class PlayerSelection : MonoBehaviour
     public void AddUnitDesignation(int index)
     {
         if (_selectedPlayers.Count == 0) return;
-        _unitDesignations[index] = new List<PlayerUnit>(_selectedPlayers);
+
+        // 지정하기 전에 현재 선택된 유닛 중 null이 아닌 유닛만 리스트에 추가
+        _unitDesignations[index] = _selectedPlayers.Where(unit => unit != null).ToList();
     }
 
     // 부대 선택 (숫자)
     public void SelectUnitDesignation(int index)
     {
-        if (!_unitDesignations.TryGetValue(index, out var unitGroup)) return;
+        if (_unitDesignations.TryGetValue(index, out var unitGroup) == false) return;
+
+        // 부대 목록에서 파괴된(null) 유닛을 제거
+        unitGroup.RemoveAll(item => item == null);
 
         DeselectAllUnit();
         foreach (var unit in unitGroup)
         {
-            if (unit == null) continue;
-            _selectedPlayers.Add(unit);
-            unit.Selected();
+            if (_selectedPlayers.Add(unit))
+            {
+                unit.Selected();
+            }
         }
     }
 
     // 유닛 명령 공통 처리
-    private void CommandSelectedUnits(Vector3 position, bool useDetector, System.Action<PlayerUnit, Vector3> command)
+    private void CommandSelectedUnits(Vector3 position, bool useDetector, Action<PlayerUnit, Vector3> command)
     {
+        // 명령을 내리기 전, 파괴된 유닛이 있다면 목록에서 제거
+        _selectedPlayers.RemoveWhere(unit => unit == null);
+
         if (_selectedPlayers.Count == 0) return;
 
-        if (_selectedPlayers.Count == 1)
-        {
-            if (useDetector) _selectedPlayers[0].OnDetector();
-            else _selectedPlayers[0].OffDetector();
+        // LINQ를 사용하여 살아있는 유닛 리스트를 만듦 (명령 처리를 위해)
+        List<PlayerUnit> aliveUnits = _selectedPlayers.ToList();
 
-            _selectedPlayers[0].MonsterTargetCancel();
-            command(_selectedPlayers[0], position);
+        if (aliveUnits.Count == 1)
+        {
+            PlayerUnit unit = aliveUnits[0];
+            if (useDetector) unit.OnDetector();
+            else unit.OffDetector();
+
+            unit.MonsterTargetCancel();
+            command(unit, position);
             return;
         }
 
-        for (int i = 0; i < _selectedPlayers.Count; i++)
+        for (int i = 0; i < aliveUnits.Count; i++)
         {
-            Vector3 offset = GetDistributedPosition(i, _selectedPlayers.Count, spreadRadius);
+            Vector3 offset = GetDistributedPosition(i, aliveUnits.Count, spreadRadius);
             Vector3 destination = position + offset;
 
-            if (useDetector) _selectedPlayers[i].OnDetector();
-            else _selectedPlayers[i].OffDetector();
+            PlayerUnit unit = aliveUnits[i];
 
-            _selectedPlayers[i].MonsterTargetCancel();
-            command(_selectedPlayers[i], destination);
+            if (useDetector) unit.OnDetector();
+            else unit.OffDetector();
+
+            unit.MonsterTargetCancel();
+            command(unit, destination);
         }
     }
 
+    // 람다식을 사용한 간결한 메서드 호출 
     public void SelectUnit_Move(Vector3 position)
         => CommandSelectedUnits(position, false, (unit, dest) => unit.Move(dest));
 
@@ -90,8 +115,12 @@ public class PlayerSelection : MonoBehaviour
 
     public void SelectUnit_Attack(IFighter monster)
     {
+        // 공격 명령 시에도 파괴된 유닛은 제외
         foreach (var player in _selectedPlayers)
-            player.AttackTargetSet(monster);
+        {
+            if (player != null)
+                player.AttackTargetSet(monster);
+        }
     }
 
     private Vector3 GetDistributedPosition(int index, int count, float radius)
